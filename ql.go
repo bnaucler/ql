ebd01c093334862f7708a42814cc5c1368760328
bnaucler <mail@bnaucler.se>
2020-05-03 19:55:38 +0200

    Initial commit

---
+++ ql.go
@@ -0,0 +1,242 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "encoding/json"
+
+    "github.com/boltdb/bolt"
+)
+
+const PORT = 9955
+const DBNAME = "./data/ql.db"
+
+var INDEX = []byte(".index")
+var LBUC = []byte(".lbuc")
+
+type Apicall struct {
+    Id string               // Id of item to process
+    List string             // List name
+    Name string             // Name of item to process
+}
+
+type Resp struct {
+    Code int                // Status code
+    Name string             // List name
+    Items []string          // Item names
+}
+
+// Log all errors
+func cherr(e error) error {
+    if e != nil { log.Println(e) }
+    return e
+}
+
+// Opens the database
+func opendb(dbname string) *bolt.DB {
+
+    db, e := bolt.Open(dbname, 0640, nil)
+    cherr(e)
+
+    return db
+}
+
+// Write JSON encoded byte slice to DB
+func wrdb(db *bolt.DB, k []byte, v []byte, cbuc []byte) (e error) {
+
+    e = db.Update(func(tx *bolt.Tx) error {
+        b, e := tx.CreateBucketIfNotExists(cbuc)
+        if e != nil { return e }
+
+        e = b.Put(k, v)
+        if e != nil { return e }
+
+        return nil
+    })
+    return
+}
+
+// Return JSON encoded byte slice from DB
+func rdb(db *bolt.DB, k []byte, cbuc []byte) (v []byte, e error) {
+
+    e = db.View(func(tx *bolt.Tx) error {
+        b := tx.Bucket(cbuc)
+        if b == nil { return fmt.Errorf("No bucket!") }
+
+        v = b.Get(k)
+        return nil
+    })
+    return
+}
+
+// Processes API call
+func getcall(r *http.Request) Apicall {
+
+    e := r.ParseForm()
+    cherr(e)
+
+    ret := Apicall{
+        Name:       r.FormValue("name"),
+        List:       r.FormValue("list"),
+        Id:         r.FormValue("id"),
+    }
+
+    return ret
+}
+
+// Reads list index from database
+func getindex(db *bolt.DB, buc []byte) Resp {
+
+    i := Resp{Code: 0}
+
+    lraw, e := rdb(db, INDEX, buc)
+    cherr(e)
+
+    e = json.Unmarshal(lraw, &i)
+    cherr(e)
+
+    return i
+}
+
+// Handles incoming requests for list index
+func getlists(w http.ResponseWriter, r *http.Request, db *bolt.DB) {
+
+    resp := getindex(db, LBUC)
+
+    fmt.Printf("DEBUG %+v\n", resp)
+
+    enc := json.NewEncoder(w)
+    enc.Encode(resp)
+}
+
+// Writes list index to database
+func wrindex(db *bolt.DB, i Resp, buc []byte) {
+
+    iraw, e := json.Marshal(i)
+    cherr(e)
+
+    e = wrdb(db, INDEX, iraw, buc)
+    cherr(e)
+}
+
+// Adds list to index
+func addtoindex(db *bolt.DB, name string, buc []byte) {
+
+    iraw, e := rdb(db, INDEX, buc)
+    cherr(e)
+
+    i := Resp{}
+    e = json.Unmarshal(iraw, &i)
+
+    i.Items = append(i.Items, name)
+    wrindex(db, i, buc)
+
+    fmt.Printf("DEBUG addtoindex: %+v\n", i)
+}
+
+// Returns correct bucket based on name
+func getbuc(c Apicall) []byte {
+
+    var buc []byte
+
+    if len(c.List) < 1 {
+        buc = LBUC
+    } else {
+        buc = []byte(c.List)
+    }
+
+    fmt.Printf("DEBUG c.List: |%+v|\n", c.List)
+    fmt.Printf("DEBUG bucket ret: %+v\n", string(buc))
+
+    return buc
+}
+
+// Handles incoming requests to add lists
+func additem(w http.ResponseWriter, r *http.Request, db *bolt.DB) {
+
+    c := getcall(r)
+    buc := getbuc(c)
+
+    var name = []byte(c.Name)
+
+    e := wrdb(db, name, name, buc)
+    cherr(e)
+
+    addtoindex(db, c.Name, buc)
+
+    resp := getindex(db, buc)
+
+    fmt.Printf("DEBUG %+v\n", c)
+    fmt.Printf("DEBUG %+v\n", resp)
+
+    enc := json.NewEncoder(w)
+    enc.Encode(resp)
+}
+
+// Removes a list from index
+func rmitem(w http.ResponseWriter, r *http.Request, db *bolt.DB) {
+
+    c := getcall(r)
+    buc := getbuc(c)
+
+    lindex := getindex(db, buc)
+    nlindex := Resp{}
+
+    for _, v := range lindex.Items {
+        if v != c.Name {
+            nlindex.Items = append(nlindex.Items, v)
+        }
+    }
+
+    wrindex(db, nlindex, buc)
+
+    enc := json.NewEncoder(w)
+    enc.Encode(nlindex)
+
+    fmt.Printf("DEBUG remove %+v\n", c)
+}
+
+// Opens a list and returns contents
+func openlist(w http.ResponseWriter, r *http.Request, db *bolt.DB) {
+
+    c := getcall(r);
+
+    resp := getindex(db, []byte(c.List))
+    resp.Name = c.List
+    resp.Code = 1
+
+    fmt.Printf("DEBUG open %+v\n", resp)
+
+    enc := json.NewEncoder(w)
+    enc.Encode(resp)
+}
+
+func main() {
+
+    // Static content
+    http.Handle("/", http.FileServer(http.Dir("./static")))
+
+    db := opendb(DBNAME)
+    defer db.Close()
+
+    http.HandleFunc("/getlists", func(w http.ResponseWriter, r *http.Request) {
+        getlists(w, r, db)
+    })
+
+    http.HandleFunc("/additem", func(w http.ResponseWriter, r *http.Request) {
+        additem(w, r, db)
+    })
+
+    http.HandleFunc("/rmitem", func(w http.ResponseWriter, r *http.Request) {
+        rmitem(w, r, db)
+    })
+
+    http.HandleFunc("/openlist", func(w http.ResponseWriter, r *http.Request) {
+        openlist(w, r, db)
+    })
+
+    lport := fmt.Sprintf(":%d", PORT)
+    e := http.ListenAndServe(lport, nil)
+    cherr(e)
+}
---
+++ static/js/ql.js
@@ -0,0 +1,106 @@
+var clist = "";
+
+// Initialization of ql, called from index.html
+function qlinit() {
+    let req = "/getlists";
+    mkxhr(req, procresp);
+}
+
+// Returns clist request string if applicable
+function getclist() {
+
+    if(clist === undefined || clist == "") return ""
+    else return "&list=" + clist;
+}
+
+// Adds item to current list
+function additem() {
+
+    let i = document.getElementById("additem").value;
+    var req = "/additem?name=" + i + getclist();
+    mkxhr(req, procresp);
+
+    console.log("Additem req: " + req);
+    console.log("Adding item: " + i);
+}
+
+// Removes item
+function rmitem(e) {
+
+    console.log("Remove: " + e.parentNode.innerText);
+
+    let req = "/rmitem?name=" + e.parentNode.innerText + getclist();
+    mkxhr(req, procresp);
+}
+
+// Opens selected list
+function openlist(e) {
+
+    console.log("Open: " + e.innerText);
+
+    let req = "/openlist?list=" + e.innerText;
+    mkxhr(req, procresp);
+}
+
+// Returns to home screen
+function reset() {
+    clearui();
+    clist = "";
+
+    let url = "/getlists";
+    mkxhr(url, procresp);
+}
+
+// Creates XHR and calls rfunc with response
+function mkxhr(dest, rfunc) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", dest, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            rfunc(xhr);
+        }
+    }
+
+    xhr.send();
+}
+
+// Removes all listed items in UI
+function clearui() {
+    document.getElementById("ui").innerHTML = "";
+}
+
+// Adds item to UI
+function adduiitem(id, name) {
+    var ui = document.getElementById("ui");
+    let item = document.createElement("div");
+    let txt = document.createTextNode(name);
+    let rm = document.createElement("div");
+
+    item.classList.add("item");
+    item.onclick = function() { openlist(this); };
+
+    rm.classList.add("rm");
+    rm.onclick = function() { rmitem(this); };
+
+    item.appendChild(txt);
+    item.appendChild(rm);
+    ui.appendChild(item);
+}
+
+// Processes lists request response
+function procresp(resp) {
+
+    var j = JSON.parse(resp.responseText);
+    clearui();
+
+    console.log("DEBUG procresp: " + j);
+
+    if(j.Code == 1) clist = j.Name;
+
+    for(l of j.Items) {
+        adduiitem(0, l);
+    }
+}
